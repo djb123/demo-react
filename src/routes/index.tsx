526202afb88c9e5268b18290b318b708da53d16f
David Beall <David.Beall@Tuvoli.com>
2026-02-17 10:59:55 -0500

    removing App component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const [count, setCount] = useState(0)
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
     </div>
   )
 }
